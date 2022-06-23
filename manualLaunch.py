4c10648e825eca2140cd348cce8cbb13226ff0e3
Addie <adiorio@nceschool.org>
2022-06-23 11:17:47 -0400

    HI set shootWheel and belts

--- manualLaunch.py
+++ manualLaunch.py
@@ -1,5 +1,29 @@
 import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
-def main(robot: libhousy.robot, controller: libhousy.controller):
-    #Here is where your recurring code will go
-    print("Hello World!")
+def launch(robot):
+    robot.beltZ1.Set(-0.8)
+    robot.beltZ2.Set(-0.8)
+    robot.beltZ3.Set(1)
+    robot.upperTension.Extend()
+    robot.lowerTension.Retract()
+
+
+
+
+def main(robot: libhousy.robot):
+#Here is where your recurring code will go
+    if robot.controller.getButton(robot.controller.Button.rBumper):
+        robot.shootWheel.Set(1)
+
+    else:
+        robot.shootWheel.Set(0)
+
+    if (robot.controller.getAxis(robot.controller.Axis.rTrigger) > 0.8 and robot.controller.getButton(robot.controller.Button.rBumper)):
+        launch(robot)
+
+    else:
+        robot.beltZ1.Set(0)
+        robot.beltZ2.Set(0)
+        robot.beltZ3.Set(0)
+       
+    
