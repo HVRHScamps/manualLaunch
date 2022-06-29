118f7464912381d265ebaf34cf1218c675f770bd
space-muffin <bbell09@icloud.com>
2022-06-29 11:57:43 -0400

    fixed

--- manualLaunch.py
+++ manualLaunch.py
@@ -5,21 +5,19 @@ def main(robot: libhousy.robot):
     # Here is where your recurring code will go
     if  robot.controller.getButton(robot.controller.Button.rBumper):
         robot.shootWheel.Set(1)
-    else:
-        robot.shootWheel.Set(0)
-
         if robot.controller.getAxis(robot.controller.Axis.rTrigger) >=.8:
             robot.beltZ1.Set(-0.8)
             robot.beltZ2.Set(-0.8)
             robot.beltZ3.Set(1)
             robot.upperTension.Extend()
             robot.lowerTension.Retract()
-        else:
-            robot.pickupMotor.Set(0)
-            robot.pickupPneumatic.Retract()
-            robot.beltZ1.Set(0)
-            robot.beltZ2.Set(0)
-            robot.beltZ3.Set(0)
+    else:
+        robot.shootWheel.Set(0)
+        robot.pickupMotor.Set(0)
+        robot.pickupPneumatic.Retract()
+        robot.beltZ1.Set(0)
+        robot.beltZ2.Set(0)
+        robot.beltZ3.Set(0)
 
     
 
