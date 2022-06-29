d1ffaf0f5c99ac9caa3f7fad0d79549599e1eb12
space-muffin <bbell09@icloud.com>
2022-06-29 11:29:01 -0400

    belts and launch weel added

--- manualLaunch.py
+++ manualLaunch.py
@@ -3,7 +3,30 @@ import libhousy
 
 def main(robot: libhousy.robot):
     # Here is where your recurring code will go
-    print("Hello World!")
+    robot.controller.getAxis(robot.controller.Axis.rTrigger)
+    robot.controller.getButton(robot.controller.Button.rBumper)
+    robot.shootWheel.Set(1)
+    if  robot.controller.getAxis(robot.controller.Axis.rTrigger) >=.8:
+        robot.shootWheel.Set(1)
+    else:
+        robot.shootWheel.Set(0)
+
+        if robot.controller.getButton(robot.controller.Button.rBumper):
+            robot.beltZ1.Set(-0.8)
+            robot.beltZ2.Set(-0.8)
+            robot.beltZ3.Set(1)
+            robot.upperTension.Extend()
+            robot.lowerTension.Retract()
+        else:
+            robot.pickupMotor.Set(0)
+            robot.pickupPneumatic.Retract()
+            robot.beltZ1.Set(0)
+            robot.beltZ2.Set(0)
+            robot.beltZ3.Set(0)
+
     
+
+    
+
     # After everything is done, we tell the main program to stop us
-    return libhousy.DONE
+
